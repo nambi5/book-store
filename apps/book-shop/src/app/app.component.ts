864e330c8eddd1192364c50ebc038b54ac863d63
NRajan3 <nambi.rajan3@t-mobile.com>
2020-11-06 14:20:51 +0530

    initial commit

---
+++ apps/book-shop/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'book-store-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'book-shop';
+}
