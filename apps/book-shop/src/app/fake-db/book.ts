738901923a6b76e15e4502d0008c56d85d98ff35
NRajan3 <nambi.rajan3@t-mobile.com>
2020-12-17 13:49:35 +0530

    removed model unused file

--- apps/book-shop/src/app/fake-db/book.ts
+++ apps/book-shop/src/app/fake-db/book.ts
@@ -1,21 +1,21 @@
 import { Book } from '@book-store/ui';
 
-export const fakeBookForTesting:Book = {
-    id: 'nHnOERqf-MQC',
-    title: 'India',
-      authors: ['Stanley A. Wolpert'],
-      publisher: 'Univ of California Press',
-      description:
-        '"To all of us who delightedly and sometimes repetitively call ourselves Old India hands, Stanley Wolpert is the acknowledged authority. This book tells why. Indian history, art, culture, and contemporary politics are here in accurate, wide-ranging, and lucid prose."--John Kenneth Galbraith',
-      pageCount: 273,
-      averageRating: 3.5,
-      imageLinks: {
-        smallThumbnail:
-          'http://books.google.com/books/content?id=nHnOERqf-MQC&printsec=frontcover&img=1&zoom=5&edge=curl&source=gbs_api',
-        thumbnail:
-          'http://books.google.com/books/content?id=nHnOERqf-MQC&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api',
-      },
-      language: 'en',
-      previewLink:
-        'http://books.google.co.in/books?id=nHnOERqf-MQC&printsec=frontcover&dq=india&hl=&cd=3&source=gbs_api',
-  };
+export const fakeBookForTesting: Book = {
+  id: 'nHnOERqf-MQC',
+  title: 'India',
+  authors: ['Stanley A. Wolpert'],
+  publisher: 'Univ of California Press',
+  description:
+    '"To all of us who delightedly and sometimes repetitively call ourselves Old India hands, Stanley Wolpert is the acknowledged authority. This book tells why. Indian history, art, culture, and contemporary politics are here in accurate, wide-ranging, and lucid prose."--John Kenneth Galbraith',
+  pageCount: 273,
+  averageRating: 3.5,
+  imageLinks: {
+    smallThumbnail:
+      'http://books.google.com/books/content?id=nHnOERqf-MQC&printsec=frontcover&img=1&zoom=5&edge=curl&source=gbs_api',
+    thumbnail:
+      'http://books.google.com/books/content?id=nHnOERqf-MQC&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api',
+  },
+  language: 'en',
+  previewLink:
+    'http://books.google.co.in/books?id=nHnOERqf-MQC&printsec=frontcover&dq=india&hl=&cd=3&source=gbs_api',
+};
--- apps/book-shop/src/app/models/user-details.model.ts
+++
@@ -1,6 +0,0 @@
-export interface BillingDetails {
-    name:string;
-    email: string;
-    phoneNumber: string;
-    address: string;
-  }
